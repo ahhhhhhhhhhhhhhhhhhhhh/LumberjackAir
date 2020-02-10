78aa9fff549e0ab497d273f6b779100ca518a491
Vanessa1104 <48804509+Vanessa1104@users.noreply.github.com>
2020-02-09 23:17:12 -0800

    added catapult animation

--- LumberjackAir/catpultpult.gd
+++ LumberjackAir/catpultpult.gd
@@ -1,19 +1,23 @@
-extends Node2D
+extends Sprite
+
+var rds = 60
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+	set_process(true)
 	pass # Replace with function body.
 
+func _process(delta):
+	
+	self.rotation_degrees += rds *-delta 
+	if self.rotation_degrees < -50:
+		rds = 0
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-#called by launch.gd when putting up the game scene
-func setup(score):
-	$Player.y_velocity = score / 6 #6 is completely arbitrary
-	$Player.x_velocity = score / 6
