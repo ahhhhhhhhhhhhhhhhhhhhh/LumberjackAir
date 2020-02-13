af95ca2398f57a0aba0d7191f402fa63613ebf74
Starbuck5 <46412508+Starbuck5@users.noreply.github.com>
2020-02-12 22:18:36 -0800

    Parallax!
    Reworked the game scene a bit to work with it, added mountain background image.
    
    I drew a catapult, added that to res, but its not super good
    
    I added a way to move around with the arrow keys for debugging purposes, just flip the boolean in the playercontroller script.

--- LumberjackAir/playercontroller.gd
+++ LumberjackAir/playercontroller.gd
@@ -3,31 +3,50 @@ extends KinematicBody2D
 var y_velocity = 0
 var x_velocity = 0
 
+var max_velocity = 15
+
+var manual_move_mode = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	y_velocity -= 4 * delta
-	x_velocity *= 0.995 #needs to be delta-adjusted somehow
-	x_velocity = max(x_velocity, 0) #drag can't make you go backwards
+	#moves the ground along with the player
+	get_parent().get_node("ground").position.x = self.position.x
 	
-	var bg = self.get_parent().get_node("background")
-	bg.position.x -= x_velocity
-	
-	if Input.is_action_just_pressed("jump"):
-		y_velocity += 5
-		x_velocity += 3
-		
-		var axe = load("res://axe.tscn").instance()
-		var axe_body = axe.get_node("body") #pulls out rigidbody of the axe
-		axe_body.angular_velocity = -4
-		axe_body.position = self.position
-		axe_body.position.x -= 80
-		self.get_parent().add_child(axe)
-		#self.add_child(axe)
-	
-	var collision = self.move_and_collide(Vector2(0, -y_velocity))
-	if collision:
+	if manual_move_mode:
 		y_velocity = 0
+		x_velocity = 0
+		if Input.is_key_pressed(KEY_LEFT):
+			self.position.x -= 150 * delta
+		if Input.is_key_pressed(KEY_RIGHT):
+			self.position.x += 150 * delta
+		if Input.is_key_pressed(KEY_UP):
+			self.position.y -= 150 * delta
+		if Input.is_key_pressed(KEY_DOWN):
+			self.position.y += 150 * delta
+	
+	else:
+		y_velocity -= 4 * delta
+		x_velocity *= 0.995 #needs to be delta-adjusted somehow
+		x_velocity = max(x_velocity, 0) #drag can't make you go backwards
+		
+		x_velocity = min(x_velocity, max_velocity)
+		y_velocity = min(y_velocity, max_velocity)
+		
+		if Input.is_action_just_pressed("jump"):
+			y_velocity += 5
+			x_velocity += 2
+			
+			var axe = load("res://axe.tscn").instance()
+			var axe_body = axe.get_node("body") #pulls out rigidbody of the axe
+			axe_body.angular_velocity = -4
+			axe_body.position = self.position
+			axe_body.position.x -= 80
+			self.get_parent().add_child(axe)
+		
+		var collision = self.move_and_collide(Vector2(x_velocity, -y_velocity))
+		if collision:
+			y_velocity = 0
