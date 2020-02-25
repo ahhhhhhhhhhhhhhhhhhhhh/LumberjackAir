9c537da41f2816aad2cec05da857e7f0f616879a
Starbuck5 <46412508+Starbuck5@users.noreply.github.com>
2020-02-25 09:16:15 -0800

    Made play again button work

--- LumberjackAir/game_end.gd
+++ LumberjackAir/game_end.gd
@@ -18,7 +18,7 @@ func setup(score):
 	$Ft_label.text = "You went " + str(score) + " feet"
 
 
-
 func _on_play_again_pressed():
 	get_tree().paused = false
 	get_tree().change_scene("res://launch.tscn")
+	get_node("../..").queue_free()
