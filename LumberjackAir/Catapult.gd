316e3226d58423e59941ca853bc296ab9396ba08
Starbuck5 <46412508+Starbuck5@users.noreply.github.com>
2020-03-10 09:40:06 -0700

    Do you pult, bro

--- LumberjackAir/Catapult.gd
+++ LumberjackAir/Catapult.gd
@@ -2,22 +2,19 @@ extends Node2D
 var animation_finished = false
 var rds = 0
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func start_animation():
-	rds= 60
-
-
+	rds= 100
 
 func _process(delta):
-	
-	$catapultpult.rotation_degrees += rds *-delta 
-	if $catapultpult.rotation_degrees < -50:
+	$arm.rotation_degrees += rds * delta
+	if rds > 0:
+		$rope.visible = false
+	if $arm.rotation_degrees > 43:
 		rds = 0
 		animation_finished = true
-	
 
 
