64f42d56cb92a7534330f0625f1e6414036fdbe9
Starbuck5 <charlie.c.hayden@gmail.com>
2020-04-16 18:39:40 -0700

    Starter launch animation
    For some reason I couldn't get the player to rotate while being launched.

--- LumberjackAir/catapult/Catapult.gd
+++ LumberjackAir/catapult/Catapult.gd
@@ -4,17 +4,26 @@ var rds = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func start_animation():
 	rds= 100
 
 func _process(delta):
+	var player = get_tree().get_root().get_node("Node2D/Player")
+
+	if not animation_finished:
+		player.global_position = self.get_node("arm/player_mount").global_position
+		#player.set_rotation_degrees(90)
+		#print(player.rotation_degrees)
+	
 	$arm.rotation_degrees += rds * delta
 	if rds > 0:
 		$rope.visible = false
 	if $arm.rotation_degrees > 43:
 		rds = 0
 		animation_finished = true
+		player.launched = true
+		player.get_node("guy").set_texture(preload("res://lumberjack.png"))
 
 
